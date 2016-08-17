e746af6b720dc1a7a524055f62e64493cc4c7dc7
Anthony Nguyen <anthony.nguyen@queensu.ca>
2016-08-17 01:04:00 -0400

    CORS

--- config/application.rb
+++ config/application.rb
@@ -25,13 +25,18 @@ module InstagramCollection
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-
+    config.middleware.use Rack::Cors do
+       allow do
+         origins '*'
+         resource '/api/v1/swagger.json', :headers => :any, :methods => [:get]
+       end
+    end
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
-    end
+    end    
+    config.api_only = true
   end
 end
